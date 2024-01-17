e522f67cc293543ec94529da86079e952e28b9d0
mohammed shafin <shafinms21@gmail.com>
2024-01-17 23:45:16 +0530

    routes created

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+const adminRouter = require("./routes/adminRouter");
+const userRouter = require("./routes/userRouter");
 
 const connectDb = require("./config/config");
 connectDb();
 console.log(connectDb);
+
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
 
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
