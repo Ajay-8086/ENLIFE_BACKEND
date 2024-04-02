ee882a1f79379a9b8c1d5dd9d68d5428e5de5430
ajay <tajay5767@gmail.com>
2024-04-02 20:50:30 +0530

    signup completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,82 +3,78 @@ const validation = require('../utilities/validation')
 const bcrypt = require('bcrypt')
 const sendMail  =require('../utilities/sendMail')
 module.exports = {
-    getUserSignup:(req,res)=>{
-        try {
-            res.status(200).json('Get user signup')
-        } catch (error) {
-           console.log(error); 
-        }
-    },
+ 
     postUserSignup:async(req,res)=>{
         try {
+            console.log(req.body);
             const {
-                fullName,
+                fullname,
                 email,
                 password,
                 confirmPassword,
-                mobileNumber,
+                mobile,
                 dob,
                 gender,
                 houseName,
-                citydistrict,
+                city,
+                district,
                 state,
                 pin,
                 bloodGroup,
-                last_Donated_Date
+                last_date
             } =req.body
             const userExist  = await userModel.findOne({email})
             if(userExist){
                return res.status(400).json('User already exists Please login')
             }else if(!validation.validationFields(
                 [
-                    fullName,
+                    fullname,
                     email,
                     password,
                     confirmPassword,
-                    mobileNumber,
+                    mobile,
                     dob,
                     gender,
                     houseName,
-                    citydistrict,
+                    city,
+                    district,
                     state,
                     pin,
                     bloodGroup,
                 ])){
-                return  res.status(400).json('All fields are required')
+                return  res.status(400).json({message:'All fields are required'})
               }else if(!validation.emailValidation(email)){
                   return  res.status(400).json('Invalid email format')
               }else if(!validation.pwdValidation(password)){
                   return  res.status(400).json('Invalid password format')
               }else if(!validation.confirmPwd(password,confirmPassword)){
                   return  res.status(400).json('password and confirm password should match')
-              }else if(!validation.mobileValidation(mobileNumber)){
+              }else if(!validation.mobileValidation(mobile)){
                 return  res.status(400).json('Please enter a valid mobile number')
               }else{
                 const hasPassword  =await bcrypt.hash(password,10) 
                 const newUser =new userModel({
-                fullName,
+                fullName:fullname,
                 email,
                 password:hasPassword,
-                confirmPassword,
-                mobileNumber,
+                mobileNumber:mobile,
                 dob,
                 gender,
                 houseName,
-                citydistrict,
+                city,
+                district,
                 state,
                 pin,
                 bloodGroup,
-                last_Donated_Date
+                last_Donated_Date:last_date
                 })
                 await newUser.save()
-                const generateOTP = req.session.generateOTP || Math.floor(1000 + Math.random() * 9000);
-               const hello = 'hgaaao'
+                const generateOTP = Math.floor(1000 + Math.random() * 9000);
                 await sendMail(email, `${generateOTP}`);
                 res.status(200).json('User signup successfull')
               }
         } catch (error) {
-            console.log(error.message);
+            console.log(error);
             res.status(500).json('Internal server errror')
         }
     }
